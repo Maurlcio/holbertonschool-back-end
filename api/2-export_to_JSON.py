f009bbc4a184423b0aa1da4636c937bd14e7ab08
Maurlcio <thecoolermess@gmail.com>
2023-04-02 13:28:23 -0700

    similar to the CSV exercise but in JSON format. A little less neat but a lot more convenient to code, to be honest

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Extracts data about an employee from an API"""
+from json import dump
+from requests import get
+from sys import argv
+
+if __name__ == "__main__":
+    if argv[1].isdigit() is True:
+        employee = get("https://jsonplaceholder.typicode.com/users/{}".format(
+                                                                     argv[1]))
+        employee_username = employee.json().get('username')
+
+        data = get(
+                "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+                                                                     argv[1]))
+
+        alltask = {}
+        alltask.setdefault(argv[1], [])
+        for tasks in data.json():
+            alltask[argv[1]].append(dict(
+                task=tasks['title'], completed=tasks['completed'],
+                username = employee_username))
+
+        with open(argv[1] + '.json', 'w') as file:
+            dump(alltask, file)
+
+    else:
+        print("nice try bozo")
