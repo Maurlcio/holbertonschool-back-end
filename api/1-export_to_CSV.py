825261b4496e3793dd76efb385ebed66abb04e78
Maurlcio <thecoolermess@gmail.com>
2023-04-02 13:14:27 -0700

    neat format, pretty nice

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -14,12 +14,12 @@ if __name__ == "__main__":
         data = get(
                 "https://jsonplaceholder.typicode.com/users/{}/todos".format(
                                                                      argv[1]))
-        
+
         with open(argv[1] + '.csv', 'w') as file:
             for todos in data.json():
                 writeobj = csv.writer(file, quoting=csv.QUOTE_ALL)
-                writeobj.writerow(
-                        [todos['userId'], employee_username,
-                        todos['completed'], todos['title']])
+                writeobj.writerow([
+                    todos['userId'], employee_username,
+                    todos['completed'], todos['title']])
     else:
         print("nice try bozo")
